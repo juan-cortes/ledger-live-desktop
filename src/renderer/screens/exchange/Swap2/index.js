14ffc153b188575efd2f330f46f2527ac5fba95d
jgiamporcaro-ledger <jonathan.giamporcaro@gmail.com>
2021-07-22 13:24:57 +0200

    design(swap): integrate form container
    This is a skeleton of the form page.

--- src/renderer/screens/exchange/Swap2/index.js
+++ src/renderer/screens/exchange/Swap2/index.js
@@ -1,22 +1,82 @@
 // @flow
-import React from "react";
-import { Trans } from "react-i18next";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import TrackPage from "~/renderer/analytics/TrackPage";
-import { Card } from "~/renderer/components/Box";
+import styled from "styled-components";
 import Text from "~/renderer/components/Text";
+import Box from "~/renderer/components/Box";
+import ButtonBase from "~/renderer/components/Button";
+
+const Main = styled.main`
+  display: grid;
+  grid-template-columns: 1fr 27.5rem 1fr;
+  row-gap: 1.5rem;
+
+  padding: 2rem 0;
+  background-color: ${p => p.theme.colors.palette.background.paper};
+  border-bottom-right-radius: 8px;
+  border-bottom-left-radius: 8px;
+
+  & > * {
+    // Automatically move every children in the middle column
+    grid-column-start: 2;
+    grid-column-end: 3;
+  }
+`;
+
+const Header = styled.div`
+  padding: 0 2rem;
+`;
+
+const Subtitle = styled(Text).attrs({
+  textAlign: "center",
+  ff: "Inter",
+  fontSize: "0.8125rem",
+  lineHeight: "1.4",
+})`
+  display: inline-block;
+  color: ${p => p.theme.colors.palette.text.shade50};
+`;
+
+const Body = styled(Box).attrs({
+  p: 20,
+  borderRadius: 8,
+  boxShadow: "0px 3px 10px rgba(0, 0, 0, 0.04)",
+})`
+  row-gap: 1.75rem;
+  border: 1px solid ${p => p.theme.colors.palette.action.active};
+`;
+
+const Button = styled(ButtonBase)`
+  justify-content: center;
+`;
 
 const Swap2 = () => {
+  // SWAP MOCK - PLEASE REMOVE ME ASA LOGIC IS IMPLEMENTED
+  const [isCTADisabled] = useState(false);
+  const { t } = useTranslation();
+
+  // SWAP MOCK - PLEASE REMOVE ME ASA LOGIC IS IMPLEMENTED
+  const onSubmit = () => {};
+
   return (
     <>
       <TrackPage category="Swap" />
       <Text horizontal mb={20} ff="Inter|SemiBold" fontSize={7} color="palette.text.shade100">
-        <Trans i18nKey="swap.title" />
+        {t("swap.title")}
       </Text>
-      <Card p={30}>
-        <Text ff="Inter|Regular" fontSize={4}>
-          {"This is the new and improved swap"}
-        </Text>
-      </Card>
+      <Main>
+        <Header>
+          <Subtitle>{t("swap2.subtitle")}</Subtitle>
+        </Header>
+        <Body>
+          <div>FIRST FORM SECTION {/* REMOVE ME ASA THE SECTION IS MERGED */}</div>
+          <div>SECOND FORM SECTION {/* REMOVE ME ASA THE SECTION IS MERGED */}</div>
+          <Button primary disabled={isCTADisabled} onClick={onSubmit}>
+            {t("common.exchange")}
+          </Button>
+        </Body>
+      </Main>
     </>
   );
 };
